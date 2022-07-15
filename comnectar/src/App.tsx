5c1ceed8d1655b789c55a078df7982b449cf63bc
unknown <lairtondasilva07@gmail.com>
2022-07-14 21:59:31 -0300

    FIX: corrigindo conflito

--- comnectar/src/App.tsx
+++ comnectar/src/App.tsx
@@ -6,14 +6,11 @@ import Home from './paginas/home/Home';
 import Footer from './components/estaticos/footer/Footer';
 import SobreNos from './paginas/sobreNos/SobreNos';
 import CadastroUsuario from './paginas/cadastroUsuario/CadastroUsuario';
-
 import { Provider } from 'react-redux';
 import store from './store/store';
 import CadastroCategoria from './components/categorias/cadastroCategoria/CadastroCategoria';
 import RegistroProduto from './paginas/registroProduto/RegistroProduto';
 import CadastroProduto from './components/produtos/cadastroProduto/CadastroProduto';
-
-
 import ListaCategoria from './components/categorias/listaCategoria/ListaCategoria';
 import DeletaCategoria from './components/categorias/deletaCategoria/DeletaCategoria';
 import ListaProduto from './components/produtos/listaProduto/ListaProduto';
@@ -25,11 +22,6 @@ import Contato from './paginas/contato/Contato';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import ComnecteSe from './paginas/comnecte-se/Comnecte-se';
-<<<<<<< HEAD
-=======
-
->>>>>>> 633ac770c66ac55d9b19c62cc1160548245f7bf1
-
 
 function App() {
   return (
