ef1e86ba657f7fcf6c0a15a1ee70343d00286658
Tacio <taciosfer@gmail.com>
2022-07-14 22:01:07 -0300

    Outros Conflitos

--- comnectar/src/App.tsx
+++ comnectar/src/App.tsx
@@ -6,14 +6,11 @@ import Home from './paginas/home/Home';
 import Footer from './components/estaticos/footer/Footer';
 import SobreNos from './paginas/sobreNos/SobreNos';
 import CadastroUsuario from './paginas/cadastroUsuario/CadastroUsuario';
-
 import { Provider } from 'react-redux';
 import store from './store/store';
 import CadastroCategoria from './components/categorias/cadastroCategoria/CadastroCategoria';
 import RegistroProduto from './paginas/registroProduto/RegistroProduto';
 import CadastroProduto from './components/produtos/cadastroProduto/CadastroProduto';
-
-
 import ListaCategoria from './components/categorias/listaCategoria/ListaCategoria';
 import DeletaCategoria from './components/categorias/deletaCategoria/DeletaCategoria';
 import ListaProduto from './components/produtos/listaProduto/ListaProduto';
