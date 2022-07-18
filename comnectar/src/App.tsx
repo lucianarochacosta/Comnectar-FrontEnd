732bde0ad865fbbd203d0076ad7fcfef4a625e89
unknown <luan.teixeira.saramago@gmail.com>
2022-07-18 10:21:31 -0300

    mudança de rota sobre

--- comnectar/src/App.tsx
+++ comnectar/src/App.tsx
@@ -39,7 +39,7 @@ function App() {
             <Route path="/" element={<Login />} />
             <Route path="/login" element={<Login />} />
             <Route path="/home" element={<Home />} />
-            <Route path="/sobre-nos" element={<SobreNos/>} />
+            <Route path="/sobre" element={<SobreNos/>} />
             <Route path="/produtos" element={<CompraProduto />} />
             <Route path="/cadastrousuario" element={<CadastroUsuario />} />
             <Route path="/categorias" element={<ListaCategoria />} />
