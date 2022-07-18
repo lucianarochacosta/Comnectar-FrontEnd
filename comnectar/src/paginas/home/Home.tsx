16b964f7dfbce4dcb6916c5702337ae3f4ced45a
isabelapiana <isabelapiana91@gmail.com>
2022-07-18 11:37:13 -0300

    home - estilização

--- comnectar/src/paginas/home/Home.tsx
+++ comnectar/src/paginas/home/Home.tsx
@@ -25,25 +25,6 @@ function Home() {
                 </Grid>
                 <Grid container xs={12}>
                     <Grid xs={6}>
-                        <Box paddingTop={10} display="flex" alignItems="center" flexDirection="column" justifyContent="center">
-                            <img src="https://i.imgur.com/0AKEdi9.png" className="imgh2"></img>
-                        </Box>
-                    </Grid>
-                    <Grid xs={6}>
-                        <Box paddingBottom={3} paddingTop={30} display="flex" alignItems="center" flexDirection="column" justifyContent="center">
-                            <Link to="/produtos" className="text-decorator-none">
-                                <Button variant='contained' className="botaoh3" color='primary'>Listar Produtos</Button>
-                            </Link>
-                        </Box>
-                        <Box display="flex" alignItems="center" flexDirection="column" justifyContent="center">
-                            <Link to="/categorias" className="text-decorator-none">
-                                <Button variant='contained' className="botaoh3" color='primary'>Listar Categorias</Button>
-                            </Link>
-                        </Box>
-                    </Grid>
-                </Grid>
-                <Grid container xs={12}>
-                    <Grid xs={6}>
                         <Box paddingBottom={3} paddingTop={20} display="flex" alignItems="center" flexDirection="column" justifyContent="center">
                             <Link to="/registroProduto" className="text-decorator-none">
                                 <Button variant='contained' className="botaoh3" color='primary'>Cadastrar Produtos</Button>
